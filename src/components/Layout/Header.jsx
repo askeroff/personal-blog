5c890c49726aea154df4ba4a2724260662a8fada
Javid Askerov <askerovlab@gmail.com>
2019-08-14 06:38:55 +0400

    design changes

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,31 +1,22 @@
 import React from 'react'
+import Menu from './Menu'
 
 const Header = () => (
-  <header className="portfolio--header block">
-    <div className="portfolio--heading">
-      <div className="triangle--left" />
-      <h1 className="portfolio-title block-title">
-        <a href="/">Javid Askerov</a>
-      </h1>
-      <h2 className="portfolio-subtitle">
-        Full-Stack Developer. Guitar Enthusiast.
-      </h2>
-      <div className="triangle--right" />
-    </div>
-    <div className="header--menu">
-      <ul>
-        <li className="header--menu-item">
-          <a href="/">Home</a>
-        </li>
-        <li className="header--menu-item">
-          <a href="/about">About</a>
-        </li>
-        <li className="header--menu-item">
-          <a href="/projects">Projects</a>
-        </li>
-      </ul>
-    </div>
-  </header>
+  <React.Fragment>
+    <header className="portfolio--header block">
+      <div className="portfolio--heading">
+        <div className="triangle--left" />
+        <h1 className="portfolio-title block-title">
+          <a href="/">Javid Askerov</a>
+        </h1>
+        <h2 className="portfolio-subtitle">
+          Full-Stack Developer. Guitar Enthusiast.
+        </h2>
+        <div className="triangle--right" />
+      </div>
+    </header>
+    <Menu />
+  </React.Fragment>
 )
 
 export default Header
---
+++ src/components/Layout/Menu.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Menu = () => (
+  <div className="header--menu">
+    <ul>
+      <li className="header--menu-item">
+        <a href="/">Home</a>
+      </li>
+      <li className="header--menu-item">
+        <a href="/about">About</a>
+      </li>
+      <li className="header--menu-item">
+        <a href="/projects">Projects</a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Menu
